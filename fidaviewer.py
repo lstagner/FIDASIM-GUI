0be3a3f6c5860bc679272d5e43c1e862367d31b1
Luke Stagner <lstagner@uci.edu>
2014-01-31 15:42:33 -0800

    gui with tabs and directory loader

--- fidaviewer.py
+++ fidaviewer.py
@@ -3,24 +3,51 @@
 import sys
 import fidaTools as ft
 from Tkinter import *
+from tkFileDialog import askdirectory
 import ttk
 
-root=Tk()
-root.title('FIDAviewer')
+class viewer:
 
-mainframe = ttk.Frame(root,padding="3 3 12 12")
-mainframe.grid(column=0,row=0,sticky=(N,W,E,S))
-mainframe.columnconfigure(0,weight=1)
-mainframe.rowconfigure(0,weight=1)
+    def __init__(self,parent):
 
-ttk.Button(mainframe,text='Plot Spectra',command=(lambda: ft.plot_spectra('153071H02'))).grid(column=0,row=0,sticky=W)
-ttk.Button(mainframe,text='Plot NPA',command=(lambda: ft.plot_npa('153071H02'))).grid(column=0,row=1,sticky=W)
-ttk.Button(mainframe,text='Plot Neutrals',command=(lambda: ft.plot_neutrals('153071H02'))).grid(column=0,row=2,sticky=W)
-ttk.Button(mainframe,text='Plot Weights',command=(lambda: ft.plot_fida_weights('153071H02'))).grid(column=0,row=3,sticky=W)
-ttk.Button(mainframe,text='Exit',command=(lambda: sys.exit())).grid(column=0,row=4,sticky=W)
+        parent.title('FIDAviewer')
 
-for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+        #Make MenuBar
+        self.MenuBar=Menu(parent)
+        parent.config(menu=self.MenuBar)
+        self.file=Menu(self.MenuBar,tearoff=False)
+        self.file.add_command(label='Load Run',command=(lambda: self.load_dir()))
+        self.file.add_command(label='Quit',command=(lambda: sys.exit()))
+        self.MenuBar.add_cascade(label='File',menu=self.file,underline=0)
 
-root.mainloop()
+        #Make Notebook
+        self.nb=ttk.Notebook(parent)
+        self.spectra_frame=ttk.Frame(self.nb)         
+        self.npa_frame=ttk.Frame(self.nb)         
+        self.neutrals_frame=ttk.Frame(self.nb)         
+        self.weights_frame=ttk.Frame(self.nb)
+        self.nb.add(self.spectra_frame,text='Spectra')         
+        self.nb.add(self.npa_frame,text='NPA')         
+        self.nb.add(self.neutrals_frame,text='Neutrals')         
+        self.nb.add(self.weights_frame,text='FIDA Weights')
+        self.nb.pack(expand=Y,fill=BOTH)
 
+        #Spectra Frame
+        ttk.Button(self.spectra_frame,text='Plot Spectra',command=(lambda: ft.plot_spectra(self.dir))).pack(expand=Y,fill=BOTH)
+        #NPA Frame
+        ttk.Button(self.npa_frame,text='Plot NPA',command=(lambda: ft.plot_npa(self.dir))).pack(expand=Y,fill=BOTH)
+        #Neutrals Frame
+        ttk.Button(self.neutrals_frame,text='Plot Neutrals',command=(lambda: ft.plot_neutrals(self.dir))).pack(expand=Y,fill=BOTH)
+        #Weights Frame
+        ttk.Button(self.weights_frame,text='Plot Weights',command=(lambda: ft.plot_fida_weights(self.dir))).pack(expand=Y,fill=BOTH)
+
+        self.load_dir()
+
+    def load_dir(self):
+        self.dir=askdirectory()
+
+if __name__=='__main__':
+    root=Tk()
+    viewer(root)
+    root.mainloop()
 
